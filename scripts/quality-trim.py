69e1145b8dd56c76eea390dee97d08e5936ce4b7
Adina <howead@msu.edu>
2011-02-10 12:05:48 -0500

    fixed quality-filter.py for all types of files

--- scripts/quality-trim.py
+++ scripts/quality-trim.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python
 
 import sys
+import bz2
 import screed
 from screed import fastq
+import gzip
 
 # python quality-trim.py <input fastq file> <output filtered fastq file>
 # MINLENGTH is the minimum lenth of read desired.  NCALLS is the percentage of a read with 'N' base calls for which if read has greater, it will be removed. 
 
 MINLENGTH = 30
 
-filein = sys.argv[1]
+if sys.argv[1].endswith('bz2'):
+    fp = bz2.BZ2File(sys.argv[1], 'r')
+if sys.argv[1].endswith('gz'):
+    fp = gzip.open(sys.argv[1], 'r')
+if sys.argv[1].endswith('fq'):
+    fp = open(sys.argv[1], 'r')
+
 fileout = sys.argv[2]
 
-fp = open(filein)
 fw = open(fileout, 'w')
 
 count=0
@@ -31,9 +38,9 @@ for n, record in enumerate(fastq.fastq_iter(fp)):
 
         if trim > MINLENGTH or (trim == -1 and len(sequence) > MINLENGTH):
             if trim == -1:
-                fw.write('@%s\n%s\n+\n%s\n' % (name, sequence, accuracy))
+                fw.write('>%s\n%s\n' % (name, sequence))
             else:
-                fw.write('@%s\n%s\n+\n%s\n' % (name, sequence[:trim], accuracy[:trim]))
+                fw.write('>%s\n%s\n' % (name, sequence[:trim]))
             count += 1
 
     if n % 1000 == 0:
